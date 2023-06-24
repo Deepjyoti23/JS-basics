6e3ce0b54c6aacd8359d3b8166635f0cbb51d3f7
Deepjyoti23 <deepjyoti0401@gmail.com>
2023-06-25 00:51:15 +0530

    ..

--- index.js
+++ index.js
@@ -34,12 +34,79 @@
 // let sum = firstCard + secondCard;
 // console.log(sum);
 
-let age = 20;
-// if(age<21){
-//     console.log("You can not enter the club!");
+// let age = 20;
+// // if(age<21){
+// //     console.log("You can not enter the club!");
 
-// }else{
-//     console.log("Welcome!");
+// // }else{
+// //     console.log("Welcome!");
+// // }
+
+// age < 21 ? console.log("You can not enter the club!") : console.log("Welcome!");
+
+// for(let i=10; i<101; i+=10)
+//     console.log(i 32);
+
+// let player1Time = 102;
+// let player2Time = 107;
+
+// function getTotalRaceTime() {
+//     return player1Time + player2Time;
 // }
 
-age < 21 ? console.log("You can not enter the club!") : console.log("Welcome!");
+// let fastestRace = getTotalRaceTime();
+// console.log(fastestRace);
+
+// let ceiled =Math.ceil(4.45684)
+// console.log(ceiled);
+
+// let randomNumber = Math.floor(Math.random()*6)+1
+// console.log(randomNumber);
+
+// let player ={
+//      name : "DJ",
+//      chips : 145,
+//      sayHello: function(){
+//         console.log("H i isaan!");
+//      }
+// }
+// player.sayHello();
+
+// let person = {
+//     name: "Deepjoti",
+//     age: 24,
+//     country: "India"
+// };
+// function logData() {
+//     return person.name + " is " + person.age + " years old and lives in " + person.country;
+// }
+
+// console.log(logData());
+
+// let age=15;
+// if(age<6)
+//     console.log("free");
+// else if(age>=6 && age<=17)
+//     console.log("child discount");
+// else if(age>=18 && age <=26)
+//     console.log("student discount");
+// else if (age>=27 && age<=66)
+//     console.log("full price");
+// else
+//     console.log("senior citizen discount");
+
+// let largeCountries=["Tuvalu","India","USA","Indonesia","Monaco"];
+// console.log(largeCountries);
+// largeCountries.pop();
+// largeCountries.push("Pakistan");
+// console.log(largeCountries);
+// largeCountries.shift();
+// largeCountries.unshift("china")
+// console.log(largeCountries);
+
+let hands = ["rock","paper","scissor"];
+
+function randomArray(){
+    return hands[Math.floor(Math.random()*3)]
+}
+console.log(randomArray());
