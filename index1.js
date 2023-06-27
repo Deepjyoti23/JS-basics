f4f16e11c82e5ed949d191b5a921ddaadba747de
Deepjyoti23 <deepjyoti0401@gmail.com>
2023-06-27 17:55:41 +0530

    ..

--- index1.js
+++ index1.js
@@ -1,11 +1,28 @@
-const arr= [2,3,4,5]
-function sum(target){
+// const arr= [2,3,4,5]
+// function sum(target){
+//     for(let i=0; i<arr.length; i++){
+//             for(let j=i+1; j<arr.length; j++){
+//                 if(arr[i] + arr[j]===target)
+//                     return [arr.indexOf(arr[i]),arr.indexOf(arr[j])]
+//             }
+//     }
+// }
+
+// console.log(sum(7));
+
+let count=0;
+let fruits ="";
+let sentence;
+function gernerateSentence(desc,arr){
+    sentence = `The ${arr.length} ${desc} are `
     for(let i=0; i<arr.length; i++){
-            for(let j=i+1; j<arr.length; j++){
-                if(arr[i] + arr[j]===target)
-                    return [arr.indexOf(arr[i]),arr.indexOf(arr[j])]
-            }
+        if(i===arr.length-1){
+            sentence+=arr[i]+"."
+        }else{
+            sentence+=arr[i]+", "
+        }    
     }
+    return sentence;
 }
 
-console.log(sum(7));
+console.log(gernerateSentence("best fruits",["apple","banana"]));
